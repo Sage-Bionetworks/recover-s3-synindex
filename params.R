11fedc9f9c8250c4aa86fc1b9b38caa80e95fc6d
Meghasyam <meghasyam@sagebase.org>
2023-06-05 07:24:42 -0700

    Update params.R
    change params to production params as defined in https://sagebionetworks.jira.com/wiki/spaces/IBC/pages/2863202330/ETL-388+RECOVER+S3+Buckets+Design

--- params.R
+++ params.R
@@ -2,10 +2,10 @@
 ## Required Parameters
 ################################
 
-INGRESS_BUCKET = 'sc-237179673806-pp-uogqfh4kldop4-s3bucket-152jqu6hxr1zt' 
+INGRESS_BUCKET = 'recover-ingestion' 
 # S3 bucket whose objects are being copied: recover-dev-ingestion
 
-PRE_ETL_BUCKET = 'sc-237179673806-pp-5yw6pwnyh6ue6-s3bucket-v7r5g2lwvu7y'
+PRE_ETL_BUCKET = 'recover-input-data'
 # S3 bucket to where objects are being copied into: recover-dev-input-data
 
 AWS_DOWNLOAD_LOCATION = './temp_aws/staging/'
@@ -14,8 +14,8 @@ AWS_DOWNLOAD_LOCATION = './temp_aws/staging/'
 FILE_LIST_OUTPUT = 's3files.txt' 
 # file name of the file where the aws ls output is saved
 
-SYNAPSE_PARENT_ID = 'syn51517390'
+SYNAPSE_PARENT_ID = 'syn51406698'
 # Synapse location where the S3 bucket objects are listed
 
-SYNAPSE_FILEVIEW_ID = 'syn51399596'
+SYNAPSE_FILEVIEW_ID = 'syn51406705'
 # Synapse ID of the fileview containing list of all currentlty indexed S3 objects in Synapse
