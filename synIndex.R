4f16a7ac82a5e7dcc5da857f3c521a0906da02c7
pranavanba <pranavanba@gmail.com>
2023-07-12 17:17:04 +0000

    Apply conditional logic depending on if sts_synindex script is run prior

--- synIndex.R
+++ synIndex.R
@@ -25,62 +25,120 @@ source('~/recover-s3-synindex/awscli_utils.R')
 #############
 source('~/recover-s3-synindex/params.R')
 
-###########
-## Get a list of all files to upload and their synapse locations(parentId) 
-###########
-STR_LEN_AWS_DOWNLOAD_LOCATION = stringr::str_length(AWS_DOWNLOAD_LOCATION)
-
-## All files present locally from manifest
-synapse_manifest <- read.csv('./current_manifest.tsv', sep = '\t', stringsAsFactors = F) %>% 
-  dplyr::filter(path != paste0(AWS_DOWNLOAD_LOCATION,'owner.txt')) %>%  # need not create a dataFileHandleId for owner.txt
-  dplyr::rowwise() %>% 
-  dplyr::mutate(file_key = stringr::str_sub(string = path, start = STR_LEN_AWS_DOWNLOAD_LOCATION+1)) %>% # location of file from home folder of S3 bucket
-  dplyr::mutate(s3_file_key = paste0('main/', file_key)) %>% # the namespace for files in the S3 bucket is S3::bucket/main/
-  dplyr::mutate(md5_hash = as.character(tools::md5sum(path))) %>% 
-  dplyr::ungroup()
-
-## All currently indexed files in Synapse
-synapse_fileview <- synapser::synTableQuery(paste0('SELECT * FROM ', SYNAPSE_FILEVIEW_ID))$asDataFrame()
-
-## find those files that are not in the fileview - files that need to be indexed
-synapse_manifest_to_upload <- synapse_manifest %>% 
-  dplyr::anti_join(synapse_fileview %>% 
-                     dplyr::select(parent = parentId,
-                                   s3_file_key = dataFileKey,
-                                   md5_hash = dataFileMD5Hex))
-
-#############
-# Index in Synapse
-#############
-## For each file index it in Synapse given a parent synapse folder
-if(nrow(synapse_manifest_to_upload) > 0){ # there are some files to upload
-  for(file_number in seq(nrow(synapse_manifest_to_upload))){
-    
-    # file and related synapse parent id 
-    file_= synapse_manifest_to_upload$path[file_number]
-    parent_id = synapse_manifest_to_upload$parent[file_number]
-    s3_file_key = synapse_manifest_to_upload$s3_file_key[file_number]
-    # this would be the location of the file in the S3 bucket, in the local it is at {AWS_DOWNLOAD_LOCATION}/
-    
-    absolute_file_path <- tools::file_path_as_absolute(file_) # local absolute path
-    
-    temp_syn_obj <- synapser::synCreateExternalS3FileHandle(
-      bucket_name = PRE_ETL_BUCKET,
-      s3_file_key = s3_file_key, #
-      file_path = absolute_file_path,
-      parent = parent_id
-    )
-    
-    # synapse does not accept ':' (colon) in filenames, so replacing it with '_colon_'
-    new_fileName <- stringr::str_replace_all(temp_syn_obj$fileName, ':', '_colon_')
-    
-    f <- File(dataFileHandleId=temp_syn_obj$id,
-              parentId=parent_id,
-              name = new_fileName) ## set the new file name
-    
-    f <- synStore(f)
-    
+if (SCRIPT_PROCESS=='sts_synindex') {
+  ###########
+  ## Get a list of all files to upload and their synapse locations(parentId) 
+  ###########
+  STR_LEN_AWS_DOWNLOAD_LOCATION = stringr::str_length(AWS_PARQUET_DOWNLOAD_LOCATION)
+  
+  ## All files present locally from manifest
+  synapse_manifest <- read.csv('./current_manifest.tsv', sep = '\t', stringsAsFactors = F) %>% 
+    dplyr::filter(path != paste0(AWS_PARQUET_DOWNLOAD_LOCATION,'owner.txt')) %>%  # need not create a dataFileHandleId for owner.txt
+    dplyr::rowwise() %>% 
+    dplyr::mutate(file_key = stringr::str_sub(string = path, start = STR_LEN_AWS_DOWNLOAD_LOCATION+1)) %>% # location of file from home folder of S3 bucket
+    dplyr::mutate(s3_file_key = paste0('main/', file_key)) %>% # the namespace for files in the S3 bucket is S3::bucket/main/
+    dplyr::mutate(md5_hash = as.character(tools::md5sum(path))) %>% 
+    dplyr::ungroup()
+  
+  ## All currently indexed files in Synapse
+  synapse_fileview <- synapser::synTableQuery(paste0('SELECT * FROM ', PARQUET_FILEVIEW_ID))$asDataFrame()
+  
+  ## find those files that are not in the fileview - files that need to be indexed
+  synapse_manifest_to_upload <- synapse_manifest %>% 
+    dplyr::anti_join(synapse_fileview %>% 
+                       dplyr::select(parent = parentId,
+                                     s3_file_key = dataFileKey,
+                                     md5_hash = dataFileMD5Hex))
+  
+  #############
+  # Index in Synapse
+  #############
+  ## For each file index it in Synapse given a parent synapse folder
+  if(nrow(synapse_manifest_to_upload) > 0){ # there are some files to upload
+    for(file_number in seq(nrow(synapse_manifest_to_upload))){
+      
+      # file and related synapse parent id 
+      file_= synapse_manifest_to_upload$path[file_number]
+      parent_id = synapse_manifest_to_upload$parent[file_number]
+      s3_file_key = synapse_manifest_to_upload$s3_file_key[file_number]
+      # this would be the location of the file in the S3 bucket, in the local it is at {AWS_PARQUET_DOWNLOAD_LOCATION}/
+      
+      absolute_file_path <- tools::file_path_as_absolute(file_) # local absolute path
+      
+      temp_syn_obj <- synapser::synCreateExternalS3FileHandle(
+        bucket_name = PARQUET_BUCKET,
+        s3_file_key = s3_file_key, #
+        file_path = absolute_file_path,
+        parent = parent_id
+      )
+      
+      # synapse does not accept ':' (colon) in filenames, so replacing it with '_colon_'
+      new_fileName <- stringr::str_replace_all(temp_syn_obj$fileName, ':', '_colon_')
+      
+      f <- File(dataFileHandleId=temp_syn_obj$id,
+                parentId=parent_id,
+                name = new_fileName) ## set the new file name
+      
+      f <- synStore(f)
+      
+    }
+  }
+} else {
+  ###########
+  ## Get a list of all files to upload and their synapse locations(parentId) 
+  ###########
+  STR_LEN_AWS_DOWNLOAD_LOCATION = stringr::str_length(AWS_DOWNLOAD_LOCATION)
+  
+  ## All files present locally from manifest
+  synapse_manifest <- read.csv('./current_manifest.tsv', sep = '\t', stringsAsFactors = F) %>% 
+    dplyr::filter(path != paste0(AWS_DOWNLOAD_LOCATION,'owner.txt')) %>%  # need not create a dataFileHandleId for owner.txt
+    dplyr::rowwise() %>% 
+    dplyr::mutate(file_key = stringr::str_sub(string = path, start = STR_LEN_AWS_DOWNLOAD_LOCATION+1)) %>% # location of file from home folder of S3 bucket
+    dplyr::mutate(s3_file_key = paste0('main/', file_key)) %>% # the namespace for files in the S3 bucket is S3::bucket/main/
+    dplyr::mutate(md5_hash = as.character(tools::md5sum(path))) %>% 
+    dplyr::ungroup()
+  
+  ## All currently indexed files in Synapse
+  synapse_fileview <- synapser::synTableQuery(paste0('SELECT * FROM ', SYNAPSE_FILEVIEW_ID))$asDataFrame()
+  
+  ## find those files that are not in the fileview - files that need to be indexed
+  synapse_manifest_to_upload <- synapse_manifest %>% 
+    dplyr::anti_join(synapse_fileview %>% 
+                       dplyr::select(parent = parentId,
+                                     s3_file_key = dataFileKey,
+                                     md5_hash = dataFileMD5Hex))
+  
+  #############
+  # Index in Synapse
+  #############
+  ## For each file index it in Synapse given a parent synapse folder
+  if(nrow(synapse_manifest_to_upload) > 0){ # there are some files to upload
+    for(file_number in seq(nrow(synapse_manifest_to_upload))){
+      
+      # file and related synapse parent id 
+      file_= synapse_manifest_to_upload$path[file_number]
+      parent_id = synapse_manifest_to_upload$parent[file_number]
+      s3_file_key = synapse_manifest_to_upload$s3_file_key[file_number]
+      # this would be the location of the file in the S3 bucket, in the local it is at {AWS_DOWNLOAD_LOCATION}/
+      
+      absolute_file_path <- tools::file_path_as_absolute(file_) # local absolute path
+      
+      temp_syn_obj <- synapser::synCreateExternalS3FileHandle(
+        bucket_name = PRE_ETL_BUCKET,
+        s3_file_key = s3_file_key, #
+        file_path = absolute_file_path,
+        parent = parent_id
+      )
+      
+      # synapse does not accept ':' (colon) in filenames, so replacing it with '_colon_'
+      new_fileName <- stringr::str_replace_all(temp_syn_obj$fileName, ':', '_colon_')
+      
+      f <- File(dataFileHandleId=temp_syn_obj$id,
+                parentId=parent_id,
+                name = new_fileName) ## set the new file name
+      
+      f <- synStore(f)
+      
+    }
   }
 }
-
-
