1d42a97a3ceffa5a4fbbb7a3f41c7e81d2f1aa94
pranavanba <pranavanba@gmail.com>
2023-07-12 17:16:02 +0000

    Add distinct params to identify script process (this script) and AWS local sync location

--- sts_synindex.R
+++ sts_synindex.R
@@ -4,10 +4,11 @@ library(dplyr)
 
 synapser::synLogin(authToken = Sys.getenv('SYNAPSE_AUTH_TOKEN'))
 
+SCRIPT_PROCESS <- 'sts_synindex'
 PARQUET_BUCKET <- 'recover-processed-data'
 PARQUET_BUCKET_BASE_KEY <- 'main/parquet'
 PARQUET_FOLDER <- "syn51406699"
-AWS_DOWNLOAD_LOCATION <- './temp_aws_parquet'
+AWS_PARQUET_DOWNLOAD_LOCATION <- './temp_aws_parquet'
 SYNAPSE_PARENT_ID <- 'syn51406699'
 
 # Get STS credentials
@@ -27,11 +28,9 @@ Sys.setenv('AWS_ACCESS_KEY_ID'=token$accessKeyId,
            'AWS_SECRET_ACCESS_KEY'=token$secretAccessKey,
            'AWS_SESSION_TOKEN'=token$sessionToken)
 
-sync_cmd <- glue::glue('aws s3 sync {base_s3_uri} {AWS_DOWNLOAD_LOCATION} --exclude "*owner.txt*" --exclude "*archive*"')
+sync_cmd <- glue::glue('aws s3 sync {base_s3_uri} {AWS_PARQUET_DOWNLOAD_LOCATION} --exclude "*owner.txt*" --exclude "*archive*"')
 system(sync_cmd)
 
 SYNAPSE_AUTH_TOKEN <- Sys.getenv('SYNAPSE_AUTH_TOKEN')
-manifest_cmd <- glue::glue('SYNAPSE_AUTH_TOKEN="{SYNAPSE_AUTH_TOKEN}" synapse manifest --parent-id {SYNAPSE_PARENT_ID} --manifest ./current_manifest.tsv {AWS_DOWNLOAD_LOCATION}')
+manifest_cmd <- glue::glue('SYNAPSE_AUTH_TOKEN="{SYNAPSE_AUTH_TOKEN}" synapse manifest --parent-id {SYNAPSE_PARENT_ID} --manifest ./current_manifest.tsv {AWS_PARQUET_DOWNLOAD_LOCATION}')
 system(manifest_cmd)
-
-
